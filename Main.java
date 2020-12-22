9d99e4a3a2dbf5cc85de10528a4cd7920d500ad8
maksim <maksimsar1@gmail.com>
2020-12-22 21:04:00 +0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void Main(String[] args) {
+
+
+        int price = 100;
+        int amaunt = 900;
+        int bonus = amaunt / 100;
+        int bonus2 = 0;
+        int total = amaunt + bonus;
+        int limit = 1000;
+        if (amaunt < limit) {
+            bonus = bonus2;
+        }
+
+
+        System.out.println(bonus);
+
+
+}
+
+}
